2cbbb8ce344dfddec19a9b40e17f39a550face31
Khubaib Ahmad <21KAhmad@heckgrammar.co.uk>
2024-07-09 15:16:16 +0100

    the main sub got some errors in it, need to do validation and multiplication apporach to bin to dec

--- Program.cs
+++ Program.cs
@@ -19,37 +19,63 @@ namespace Y9_DEC_TO_BIN_SKELETON
 
             string myString2 = "I'm ";
             string bigString = myString2 + myString;
-            Console.WriteLine(bigString);
 
-            //--------------------------------------------------------------------------------------------------------------------------------------------------------
+            //----------------------------------------------------------------------------------------------------------------------------------------------------
 
-            Console.WriteLine("Enter a denary number");
-            string number = Console.ReadLine();
-            Console.WriteLine("Enter the numberbase you want to convert to");
-            string numberbase = Console.ReadLine();
-            bool validation = (numberValidation(number));
-            if (validation != true)
+
+            Console.WriteLine("Enter 1 to convert from binary to denary");
+            Console.WriteLine("Enter 2 to convert from denary to binary");
+            Console.WriteLine("Enter 3 to convert from hex to denary");
+            Console.WriteLine("Enter 4 to convert from hex to binary");
+            Console.WriteLine("Enter 5 to convert from binary to hex");
+            int chosenConversion = Console.Read();
+            if (chosenConversion == 1)
             {
-                int number = Convert.ToInt32(number);
-                int numberbase = Convert.ToInt32(numberbase);
-                Console.WriteLine(numberConversion(number, numberbase));
-            }
-            else
-            {
-                Console.WriteLine("Invalid Format");
-            }
 
+            }
+            if (chosenConversion == 2)
+            {
+                Console.WriteLine("Enter a denary number between 0 and 256");
+                string number = (Console.ReadLine());
+                int numberbase = 2;
+                while (validation(number) = false)
+                {
+                    Console.WriteLine("Please enter a denery number grater than 0");
+                    number = (Console.ReadLine());
+                }
+                number = Convert.ToInt32(number);
+                Console.WriteLine(denaryToBinary(number, numberbase));
+            }
+            if (chosenConversion == 3)
+            {
+
+            }
+            if (chosenConversion == 4)
+            {
+
+            }
+            if (chosenConversion == 5)
+            {
+
+
+
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------------------------------------------------------
+        //static void means the function will not return a value so it does not need a data type
+        //...this function DOES return a value so the method must have a data type
+
+        static bool validation(string number)
+        {
+            return false;
         }
 
-        //--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
+        static string denaryToBinary(int number, int numberbase)
         {
             int K = number;
             int count = 0;
-
+            //-----------------------------------------------------------------------------------------------------------------------------------------------
+            //This code is necessary only if you have numbers larger than 8 bytes
             while (K > 0)
             {
                 K = K / numberbase;
@@ -57,7 +83,7 @@ namespace Y9_DEC_TO_BIN_SKELETON
             }
             int[] conversion = new int[count];
             int index = count - 1;
-
+            //-------------------------------------------------------------------------------------------------------------------------------------------------
             while (number > 0)
             {
                 conversion[index] = number % numberbase;
@@ -68,20 +94,10 @@ namespace Y9_DEC_TO_BIN_SKELETON
             return result;
         }
 
-        //--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        static bool numberValidation(int number)
+        static string binaryToDenary(string number)
         {
-            bool valid = true;
-            try
-            {
-                int result = int.Parse(number);
-            }
-            catch (FormatException)
-            {
-                valid = false;
-                return valid;
-            }
+            return "";
         }
+        
     }
 }
